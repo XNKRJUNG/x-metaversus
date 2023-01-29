17b019d9052b8c1b1f8d237ba428d417c18efd2d
XNKRJUNG <shresthacyrus@outlook.com>
2023-01-29 16:39:43 +0545

    Fixes syntax errors.

--- sections/World.jsx
+++ sections/World.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 import styles from "../styles";
 import { TitleText, TypingText } from "../components";
-import { staggerContainer, fadeIn } from "../utils/motion";
+import { fadeIn, staggerContainer } from "../utils/motion";
 
 const World = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -26,21 +26,24 @@ const World = () => (
         textStyles="text-center"
       />
 
-      <motion.dev
+      <motion.div
         variants={fadeIn("up", "tween", 0.3, 1)}
         className="relative mt-[68px] flex w-full h-[550px]"
       >
         <img src="/map.png" alt="map" className="w-full h-full object-cover" />
-        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d66a0]">
+
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
           <img src="people-01.png" alt="people" className="w-full h-full" />
         </div>
-        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d66a0]">
-          <img src="people-02.png" alt="people" className="w-full h-full" />
+
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+          <img src="/people-02.png" alt="people" className="w-full h-full" />
         </div>
-        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d66a0]">
+
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
           <img src="people-03.png" alt="people" className="w-full h-full" />
         </div>
-      </motion.dev>
+      </motion.div>
     </motion.div>
   </section>
 );
